2b961536837d0cc952b94d60227cb523828fc322
codewarswarrior <alieuk61@gmail.com>
2023-02-15 22:32:07 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// const body = document.querySelector('body');
+// const bodyWrapper = document.querySelector('.body-wrapper');
+// const firstBodySec = document.querySelector('.body-section-one');
+// const secondBodySec = document.querySelector('.body-section-two');
+// let topButtons = document.querySelectorAll('.btn');
+// let changeDisplay = function(buttons){
+//   for (let i = 0; i < topButtons.length; i++){
+//     topButtons[i].style.display = 'none';
+//   }
+// }
+//
+// if (window.innerWidth <= '680'){
+//   body.style.padding = '20px'
+//   changeDisplay(topButtons);
+//   bodyWrapper.style.flexDirection = 'column';
+//   firstBodySec.style.width = '100%';
+//   secondBodySec.style.width = '100%';
+// }
