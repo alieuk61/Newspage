387c8aeaf377ff89b56ad5fdc13120c1a228d9f2
https://exzotic.co.uk <85530107+alieuk61@users.noreply.github.com>
2023-02-19 02:45:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-// const body = document.querySelector('body');
-// const bodyWrapper = document.querySelector('.body-wrapper');
-// const firstBodySec = document.querySelector('.body-section-one');
-// const secondBodySec = document.querySelector('.body-section-two');
-// let topButtons = document.querySelectorAll('.btn');
-// let changeDisplay = function(buttons){
-//   for (let i = 0; i < topButtons.length; i++){
-//     topButtons[i].style.display = 'none';
-//   }
-// }
-//
-// if (window.innerWidth <= '680'){
-//   body.style.padding = '20px'
-//   changeDisplay(topButtons);
-//   bodyWrapper.style.flexDirection = 'column';
-//   firstBodySec.style.width = '100%';
-//   secondBodySec.style.width = '100%';
-// }
+const body = document.querySelector('body');
+const desktopImg = document.querySelector('.big-pic');
+const mobileImg = document.querySelector('.hidden-img')
+const firstBigDiv = document.querySelector('.bs-one-half');
+const header = document.querySelector('.buttons');
+const menuContent = document.querySelector('.menu-content');
+const hamburger = document.querySelector('.menu');
+const closeMenu = document.querySelector('.close-menu-btn');
+
+
+hamburger.addEventListener('click', function(){
+  console.log(menuContent)
+  console.log(hamburger)
+  menuContent.classList.toggle('menu-visibility')
+  body.style.position = 'fixed';
+  body.style.backgroundColor = 'hsl(233, 8%, 79%)'
+  mobileImg.style.filter = 'brightness(0.5)'
+})
+
+closeMenu.addEventListener('click', function(){
+  menuContent.classList.toggle('menu-visibility')
+  body.style.position = 'relative';
+  body.style.backgroundColor = 'white'
+  mobileImg.style.filter = 'brightness(1)'
+})
